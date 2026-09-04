2106d8dc5fbfa2078d36d5bfae3bf24d5f06482d
itskb2026-debug <itskb2026@gmail.com>
2026-09-04 14:30:25 +0700

    Simplify index.php by removing unnecessary HTML
    Removed HTML structure and adjusted iframe style.

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-
-<html>
-   <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-      <base target="_top">
-   </head>
-   <body>
-      <iframe style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;border: 0;" src="https://script.google.com/macros/s/AKfycbzbSkIcAbUUXA4cESnxj1-MVmMqk5R2DkMFop094s9Xx4bDOqC5c7UtXfOy4IFdAELkog/exec"></iframe>
-   </body>
-</html>
+<iframe src="https://script.google.com/macros/s/AKfycbzbSkIcAbUUXA4cESnxj1-MVmMqk5R2DkMFop094s9Xx4bDOqC5c7UtXfOy4IFdAELkog/exec" style="width: 100%; height: 100vh; border: none; overflow: hidden;"></iframe>
