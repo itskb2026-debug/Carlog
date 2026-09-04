7e91c5115c26686e6318278d1c513ed6e2313584
itskb2026-debug <itskb2026@gmail.com>
2026-09-04 14:42:33 +0700

    Refactor HTML structure and iframe styling

--- Index.php
+++ Index.php
@@ -1,22 +1,12 @@
-<!doctype html>
-<html lang="th">
+<!DOCTYPE html>
+
+<html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>ระบบการใช้รถ</title>
-  <style>
-    html, body, iframe {
-      width: 100%;
-      height: 100%;
-      margin: 0;
-      border: 0;
-    }
-  </style>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<base target="_top">
+
 </head>
 <body>
-  <iframe
-    src="https://script.google.com/macros/s/AKfycbzbSkIcAbUUXA4cESnxj1-MVmMqk5R2DkMFop094s9Xx4bDOqC5c7UtXfOy4IFdAELkog/exec"
-    allow="camera"
-  ></iframe>
+<iframe style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;border: 0;" src="https://script.google.com/macros/s/AKfycbzbSkIcAbUUXA4cESnxj1-MVmMqk5R2DkMFop094s9Xx4bDOqC5c7UtXfOy4IFdAELkog/exec"></iframe>
 </body>
 </html>
