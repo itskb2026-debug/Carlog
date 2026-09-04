f3e73d4e1799ae61194a93b09e20eaecec68cba0
itskb2026-debug <itskb2026@gmail.com>
2026-09-04 15:32:59 +0700

    Refactor HTML structure and add iframe attributes

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="th">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ระบบการใช้รถ SKB</title>
+  <style>
+    html, body {
+      width: 100%;
+      height: 100%;
+      margin: 0;
+      overflow: hidden;
+    }
+
+    iframe {
+      display: block;
+      width: 100%;
+      height: 100%;
+      border: 0;
+    }
+  </style>
+</head>
+<body>
+  <iframe
+    src="https://script.google.com/macros/s/AKfycbzbSkIcAbUUXA4cESnxj1-MVmMqk5R2DkMFop094s9Xx4bDOqC5c7UtXfOy4IFdAELkog/exec"
+    allow="camera"
+    title="ระบบการใช้รถ">
+  </iframe>
+</body>
+</html>
